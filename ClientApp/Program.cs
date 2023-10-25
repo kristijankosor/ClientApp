78ed10727fa9cbd9937be41ac2a5f1b22073912e
Sorko <kristijankosor94@gmail.com>
2023-10-25 07:35:35 +0200

    Add project files.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var hubConnection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7047/locationHub")
+            .Build();
+
+        hubConnection.On<string>("ReceiveRequestInfo", (message) =>
+        {
+            Console.WriteLine($"Request was sent: {message}");
+        });
+
+        await hubConnection.StartAsync();
+        Console.WriteLine("Connected!");
+        
+        Console.ReadLine();
+        await hubConnection.StopAsync();
+    }
+}
